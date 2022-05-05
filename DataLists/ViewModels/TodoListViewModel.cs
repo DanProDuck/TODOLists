a14655b88594c32f7241837754d884576cae3d42
DanProDuck <danproducktion@gmail.com>
2022-05-05 10:44:47 +0200

    Added Models, Helper(have problem with connections), ViewModels, Controllers

---
+++ DataLists/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using DataLists.Models;
+using Dapper;
+using DataLists.Helpers;
+
+namespace DataLists.ViewModels
+{
+    public class TodoListViewModel
+    {
+        public TodoListViewModel()
+        {
+            using (var db = DbHelper.GetConnection())
+            {
+                this.EditableItem = new TodoListItem();
+                this.TodoItems = db.Query<TodoListItem>("SELECT * FROM TodoListItems ORDER BY AddDate DESC").ToList();
+            }
+        }
+
+        public List<TodoListItem> TodoItems { get; set; }
+
+        public TodoListItem EditableItem { get; set; }
+    }
+}
